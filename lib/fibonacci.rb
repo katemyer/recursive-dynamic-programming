530469525db9df0f71d9a5f7aca76ffe98c0e7f0
Chris M <chris@adadevelopersacademy.org>
2019-11-07 07:52:21 -0800

    Update fibonacci.rb

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,7 @@
 # Improved Fibonacci
 
 # Time Complexity - ?
-# Space Complexity - ?  (should be O(1))
+# Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
  
