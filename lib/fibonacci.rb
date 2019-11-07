0f3e3789cc09d58bb2d13d6bd33cdc3e4a6867f9
CheezItMan <chris@adadevelopersacademy.org>
2019-11-07 07:50:00 -0800

    lib files

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,5 +4,5 @@
 # Space Complexity - ?  (should be O(1))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
-
+ 
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,1 +1,15 @@
-# Sum of Substrings
+# Superdigit
+
+# Time Complexity - ?
+# Space Complexity - ?
+def super_digit(n)
+   
+end
+  
+
+# Time Complexity - ?
+# Space Complexity - ?
+def refined_super_digit(n, k)
+    
+end
+    
