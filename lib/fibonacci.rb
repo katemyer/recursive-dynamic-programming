e539d2a76d5b668d1a84e64d88859a9f4fd78017
kate myer <kate.d.mangubat@gmail.com>
2020-05-16 10:17:37 -0700

    edit recursive dynamic programming

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,6 +3,18 @@
 # Time Complexity - ?
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
+
 def fibonacci(n)
- 
+  raise ArgumentError if n < 0
+  return helper_method([0, 1], 2, n)
+end
+
+def helper_method(solutions, current, n)
+  return n if n == 0 || n == 1
+  return solutions[0] + solutions[1] if current == n
+  sum = solutions[0] + solutions[1]
+  solutions[0] = solutions[1]
+  solutions[1] = sum
+  
+  return helper_method(solutions, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,26 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
-end
+  string_n = n.to_s
+  return n if string_n.size == 1
+
+  i = 1
+  sum = string_n[0].to_i
+  while i < string_n.size
+    sum += string_n[i].to_i
+    i += 1
+  end
+  return super_digit(sum)
+  end
   
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+    string_n = n.to_s
+    return n if string_n.size == 1 && k == 1
+
+    repeat_n = string_n * k
+    return super_digit(repeat_n)
 end
     
